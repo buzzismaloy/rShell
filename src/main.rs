eaa1a09ff40c6b0818ef5378670d675839732336
nyxnyx <nyxnyx@archenemy>
2025-04-18 02:08:12 +0300

    Implemented simple pipes

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,63 @@ use std::process::{Child, Command, Stdio};
 fn main() {
     loop {
         print!("=> ");
-        stdout().flush();
+        stdout().flush().unwrap();
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut prev_command = None;
 
-        match command {
-            "cd" => {
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let new_path = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&new_path) {
-                    eprintln!("{}", e);
+        while let Some(command) = commands.next() {
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let new_path = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&new_path) {
+                        eprintln!("{}", e);
+                    }
+                    prev_command = None;
+                }
+
+                "exit" => return,
+
+                command => {
+                    let stdin = prev_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.unwrap())
+                    });
+
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => {
+                            prev_command = Some(output);
+                        }
+                        Err(e) => {
+                            prev_command = None;
+                            eprintln!("{}", e);
+                        }
+                    };
                 }
             }
+        }
 
-            "exit" => return,
-
-            command => {
-                let child_process = Command::new(command).args(args).spawn();
-
-                match child_process {
-                    Ok(mut child_process) => {
-                        child_process.wait();
-                    }
-                    Err(e) => eprintln!("{}", e),
-                };
-            }
+        if let Some(mut fin_command) = prev_command {
+            fin_command.wait().unwrap();
         }
     }
 }
