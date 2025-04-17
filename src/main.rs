0c59bf82be66ec4e40837d71aa20c8806acce7aa
nyxnyx <nyxnyx@archenemy>
2025-04-17 20:07:41 +0300

    Implemented wait()

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@ use std::io::{Write, stdin, stdout};
 use std::process::{Child, Command, Stdio};
 
 fn main() {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
+    loop {
+        print!("=> ");
+        stdout().flush();
 
-    let command = input.trim();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
 
-    Command::new(command).spawn().unwrap();
+        let command = input.trim();
+
+        let mut child_process = Command::new(command).spawn().unwrap();
+
+        child_process.wait();
+    }
 }
